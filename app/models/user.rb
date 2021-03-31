c1f2dc0ff4f8d5c3d14821e73e2e9443a859760c
MMORONTO <t2wycemo@gmail.com>
2021-03-31 16:42:31 -0400

    add slugify method to user model. update gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     has_many :hairstyles
+
+    before_create :slugify
+    
+    def slugify
+        self.slug = name.parametrize
+    end
+
 end
